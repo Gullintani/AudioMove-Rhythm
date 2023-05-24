a0171229c45cea01462b209d7f2e53a44b050215
Welkin Min <116010168@link.cuhk.edu.cn>
2023-05-24 17:14:39 +0900

    Add Beats Detections

--- Assets/MyScripts/AudioController.cs
+++ Assets/MyScripts/AudioController.cs
@@ -8,36 +8,34 @@ public class AudioController : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioSource Music;
-    public MobileController MobileController;
-    public List<Vector3> PositionList;
-    public float speed, tolerantAngle;
-    public bool isMoving = false;
     public Vector3 BodyPositionOffSet = new Vector3(-1.33f, 0.0f, 0.0f);
-    public List<Vector3> divisionCartesianVectors;
     public float percussionRadius = 2.0f;
-    
     void Start() {
-        
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/2.0f, Mathf.PI/3.0f));
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/2.0f, 0.0f));
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/2.0f, -Mathf.PI/3.0f));
 
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/6.0f, Mathf.PI/3.0f));
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/6.0f, 0.0f));
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/6.0f, -Mathf.PI/3.0f));
-
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/6.0f * 5.0f, Mathf.PI/3.0f));
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/6.0f * 5.0f, 0.0f));
-        divisionCartesianVectors.Add(SphericalToCartesian(percussionRadius, Mathf.PI/6.0f * 5.0f, -Mathf.PI/3.0f));
-
-        foreach (Vector3 CartesianCoordinate in divisionCartesianVectors){
-            Debug.Log(CartesianCoordinate);
-        }
+        AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+        processor.tapTempo();
+		processor.onBeat.AddListener(onOnbeatDetected);
+		processor.onSpectrum.AddListener(onSpectrum);
     }
 
-    private void Update() {
+    private void Update(){
 
     }
+    void onOnbeatDetected (){
+		Debug.Log ("Beat!!!");
+        // TeleportRandomly();
+	}
+
+	//This event will be called every frame while music is playing
+	void onSpectrum (float[] spectrum){
+		//The spectrum is logarithmically averaged
+		//to 12 bands
+		for (int i = 0; i < spectrum.Length; ++i) {
+			Vector3 start = new Vector3 (i, 0, 0);
+			Vector3 end = new Vector3 (i*5, spectrum [i] * 50, 0);
+			Debug.DrawLine (start, end);
+		}
+	}
 
     public Vector3 SphericalToCartesian(float radius, float polar, float elevation){
         float a = radius * Mathf.Cos(elevation);
@@ -45,5 +43,12 @@ public class AudioController : MonoBehaviour
         float y = radius * Mathf.Sin(elevation);
         float z = a * Mathf.Sin(polar);
         return new Vector3(x, y, z);
+    }
+
+    public void TeleportRandomly() {
+        Vector3 direction = Random.onUnitSphere;
+        direction.y = Mathf.Clamp(direction.y, 0.5f, 1.0f);
+        float distance = 2.0f * Random.value + 1.5f;
+        transform.localPosition = distance * direction;
     }
 }
